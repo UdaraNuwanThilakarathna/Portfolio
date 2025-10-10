5cf64c7c5d50aa55efce61c4821cfdac29b2c00d
UdaraNuwanThilakarathna <udaranthilakarathne2001@gmail.com>
2025-10-10 12:01:42 +0530

    grid add 4

--- src/App.jsx
+++ src/App.jsx
@@ -777,7 +777,7 @@ export default function Portfolio() {
             <div className='w-20 h-1 bg-gradient-to-r from-blue-400 to-cyan-400 mx-auto'></div>
           </div>
 
-          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+          <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
             {projects.map((project) => (
               <div
                 key={project.id}
